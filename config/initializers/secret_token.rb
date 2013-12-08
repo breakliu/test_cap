3407fa965f5edc4654db75ee604cbc34d5f4d8ef
imlcl <slackcode@gmail.com>
2013-12-08 23:20:55 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestCap::Application.config.secret_key_base = '8f38c5942591bb8e33673879f737888d9dc4addedd1c97b74703e364cc7bd8d1e782eaeb15622ea963bf8495ff24488cb3148b0e485ea7108306d15ab7863f4b'
